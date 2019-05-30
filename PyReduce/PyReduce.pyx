f838918ed5df1dbd60f419478a91b0b45430a899
Viviane Rochon Montplaisir <viviane.rochon.montplaisir@gerad.ca>
2019-05-30 15:27:21 -0400

    it works partially, now got to add back what I removed

--- PyReduce/PyReduce.pyx
+++ PyReduce/PyReduce.pyx
@@ -1,25 +1,27 @@
 # PyReduce: Integration of Reduce in Python
 
+# Cython compiler directives
 #cython: language_level=3
+#cython: boundscheck=False, wraparound=False, nonecheck=False
 
 from libcpp cimport bool
 
 
 # Define the interface function to perform optimization          
-def optimize(f, numThreads):
+def optimize(f, numThreads, nogil=True):
     cdef int runStatus
     
-    print("VRM: optimize: Call runReduce")
+    #print("VRM: optimize: Call runReduce")
     runStatus = runReduce(cb, <void*> f, numThreads)
-    print("VRM: optimize: runReduce done. returned:")
-    print(runStatus)
+    #print("VRM: optimize: runReduce done. returned:")
+    #print(runStatus)
     return runStatus
 
 
-cdef extern from "Red.hpp":
+cdef extern from "Red.hpp" nogil:
     cdef cppclass Red:
-        bool run(bool &stop)
-        #bool eval();
+        bool run(bool &stop) nogil
+        #bool eval() nogil
 
 
 cdef class PyReduceRed:
@@ -28,16 +30,16 @@ cdef class PyReduceRed:
     #    return self.c_r.run(stop)
 
 
-cdef extern from "reduceCyInterface.cpp":
+cdef extern from "reduceCyInterface.cpp" nogil:
     ctypedef int (*Callback)(void* apply, const int i)
     int runReduce(Callback cb, void* apply, const int numThreads) except+
 
 
 # Define callback function ---> link with Python     
-cdef int cb(void *f, const int i) with gil:
-    print("VRM: Calling callback cb")
-    #thread = threading.Thread(target=f, args=(u,))
-    #ret = thread.start()
-    return (<object>f)(i)
-    #return ret
+cdef int cb(void *f, const int i) nogil:
+    #print("VRM: Calling callback cb")
+    #ev = (<object>f)(i)
+    #print("VRM: Return " + str(ev))
+    #return ev
+    return 2
 
